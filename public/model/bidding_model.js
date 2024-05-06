973bb7cab7aa34fb00a33967678a25e19a12dbe9
Juan Francisco Cisneros <juanfrancistm2011@icloud.com>
2024-05-06 18:57:49 -0500

    finished

--- public/model/bidding_model.js
+++ public/model/bidding_model.js
@@ -7,7 +7,8 @@ let bidding_model= [
         year: 1503,
         price: 200_000_000,
         isSold: false,
-        lastBidID: null
+        lastBidID: null,
+        image:"http://127.0.0.1:2000/images/da_Vince_The_Mona_Lisa.jpeg"
     },
     {
         id: 1,
@@ -16,7 +17,8 @@ let bidding_model= [
         year: 1874,
         price: 10_000_000,
         isSold: false,
-        lastBidID: null
+        lastBidID: null,
+        image:"http://127.0.0.1:2000/images/Degas_Dance_Class.jpeg"
     },
     {
         id: 2,
@@ -25,7 +27,8 @@ let bidding_model= [
         year: 1876,
         price: 19_000_000,
         isSold: false,
-        lastBidID: null
+        lastBidID: null,
+        image:"http://127.0.0.1:2000/images/Degas_L_Absinthe.jpeg"
     },
     {
         id: 3,
@@ -34,7 +37,8 @@ let bidding_model= [
         year: 1918,
         price: 67_000_000,
         isSold: false,
-        lastBidID: null
+        lastBidID: null,
+        image:"http://127.0.0.1:2000/images/Modigliani_Portrait_of_Woman_in_a_Hat.jpg"
     },
     {
         id: 4,
@@ -43,7 +47,8 @@ let bidding_model= [
         year: 1919,
         price: 120_000_000,
         isSold: false,
-        lastBidID: null
+        lastBidID: null,
+        image:"http://127.0.0.1:2000/images/Modigliani_Woman_with_Red_Hair.jpeg"
     },
     {
         id:5,
@@ -52,7 +57,18 @@ let bidding_model= [
         year: 1937,
         price: 90_000_000,
         isSold: false,
-        lastBidID: null
+        lastBidID: null,
+        image:"http://127.0.0.1:2000/images/Picasso_portrait_of_Dora_Maar.jpeg"
+    },
+    {
+        id:6,
+        painter:"Van Gogh",
+        title:"Self Portrait",
+        year: 1889,
+        price: 71_000_000,
+        isSold: false,
+        lastBidID: null,
+        image:"http://127.0.0.1:2000/images/Vincent_Van_Gogh_Self_portrait.jpeg"
     }
 ];
 
